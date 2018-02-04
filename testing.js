58dbdae5242ad3465fe21bfd7e2c201580c16cc3
McHenrys <mchenryhome@CAL-9000.local>
2018-02-03 16:38:26 -0800

    testing js

--- testing.js
+++ testing.js
@@ -2,3 +2,12 @@ var canvas = document.getElementById("testing");
 var ctx = canvas.getContext("2d");
 
 console.clear();
+
+setInterval(draw, 10);
+
+var screen = []
+
+function draw() {
+	ctx.fillStyle = "#111";
+	ctx.fillRect(0, 0, 800, 600);
+}
