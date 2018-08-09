6ff2ad0333bcdd186ccf1452557fa1fb0a7507bd
Software4 <1425sw@gmail.com>
2018-08-08 18:30:54 -0700

    Add old designs

---
+++ designs/angry.js
@@ -0,0 +1,17 @@
+designs.angry.speed = 300;
+designs.angry.pattern = [
+	[
+		["#000000", "#000000", "#ff8800", "#ff8800", "#000000", "#000000", "#000000", "#000000", "#000000", "#000000", "#000000", "#000000", "#ff8800", "#ff8800", "#000000", "#000000"],
+		["#000000", "#000000", "#000000", "#ff8800", "#ff8800", "#000000", "#000000", "#000000", "#000000", "#000000", "#000000", "#ff8800", "#ff8800", "#000000", "#000000", "#000000"],
+		["#000000", "#000000", "#000000", "#000000", "#ff8800", "#ff8800", "#000000", "#000000", "#000000", "#000000", "#ff8800", "#ff8800", "#000000", "#000000", "#000000", "#000000"],
+		["#000000", "#000000", "#ffffff", "#ffffff", "#ffffff", "#ff8800", "#ff8800", "#000000", "#000000", "#ff8800", "#ff8800", "#ffffff", "#ffffff", "#ffffff", "#000000", "#000000"],
+		["#000000", "#ffffff", "#ffffff", "#ffffff", "#ffffff", "#ffffff", "#ff8800", "#000000", "#000000", "#ff8800", "#ffffff", "#ffffff", "#ffffff", "#ffffff", "#ffffff", "#000000"],
+		["#ffffff", "#ffffff", "#ffffff", "#ffffff", "#ffffff", "#ffffff", "#ffffff", "#000000", "#000000", "#ffffff", "#ffffff", "#ffffff", "#ffffff", "#ffffff", "#ffffff", "#ffffff"],
+		["#ffffff", "#ffffff", "#ffffff", "#ffffff", "#ffffff", "#ffffff", "#ffffff", "#000000", "#000000", "#ffffff", "#ffffff", "#ffffff", "#ffffff", "#ffffff", "#ffffff", "#ffffff"],
+		["#ffffff", "#ffffff", "#ffffff", "#ffffff", "#ffffff", "#ffffff", "#ffffff", "#000000", "#000000", "#ffffff", "#ffffff", "#ffffff", "#ffffff", "#ffffff", "#ffffff", "#ffffff"],
+		["#ffffff", "#ffffff", "#ffffff", "#000055", "#ffffff", "#ffffff", "#ffffff", "#000000", "#000000", "#ffffff", "#ffffff", "#ffffff", "#000055", "#ffffff", "#ffffff", "#ffffff"],
+		["#ffffff", "#ffffff", "#000055", "#000055", "#000055", "#ffffff", "#ffffff", "#000000", "#000000", "#ffffff", "#ffffff", "#000055", "#000055", "#000055", "#ffffff", "#ffffff"],
+		["#000000", "#ffffff", "#ffffff", "#ffffff", "#ffffff", "#ffffff", "#000000", "#000000", "#000000", "#000000", "#ffffff", "#ffffff", "#ffffff", "#ffffff", "#ffffff", "#000000"],
+		["#000000", "#000000", "#ffffff", "#ffffff", "#ffffff", "#000000", "#000000", "#000000", "#000000", "#000000", "#000000", "#ffffff", "#ffffff", "#ffffff", "#000000", "#000000"]
+	]
+];
